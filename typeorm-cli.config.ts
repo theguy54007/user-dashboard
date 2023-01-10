2d4e27bdd26ac2194958c93875bea5a02640dda6
tommytan <sage.ts920126@gmail.com>
2023-01-10 19:10:47 +0800

    init project

---
+++ typeorm-cli.config.ts
@@ -0,0 +1,20 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+
+config({
+  path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env',
+});
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
+  entities: ['./src/**/*.entity.ts'],
+  migrations: ['./migrations/*.ts'],
+});
