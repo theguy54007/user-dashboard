2dea4c1368a88a1b7c5a3a834644b13a07ee89be
tommytan <sage.ts920126@gmail.com>
2023-01-16 21:33:58 +0800

    se up cors and add credentials setup for cookies

--- src/nest/main.ts
+++ src/nest/main.ts
@@ -5,6 +5,10 @@ import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:4200'
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser())
   await app.listen(3000);
