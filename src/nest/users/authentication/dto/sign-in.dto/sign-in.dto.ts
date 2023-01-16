5ea693946e8de37e576624c4f2d77f7902e3d5d8
tommytan <sage.ts920126@gmail.com>
2023-01-16 21:39:36 +0800

    reorganize dto/sign-in and sign-up files

--- src/nest/users/authentication/dto/sign-in.dto/sign-in.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PickType } from '@nestjs/mapped-types';
-import { SignUpDto } from '../sign-up.dto/sign-up.dto';
-import { IsString } from 'class-validator';
-
-export class SignInDto extends PickType(SignUpDto, ['email']) {
-  @IsString()
-  password: string;
-}
