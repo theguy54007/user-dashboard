2124a62a3d281b7e716d0227bc5c0ff3c5b1e717
tommytan <sage.ts920126@gmail.com>
2023-01-16 21:45:04 +0800

    modify angular routing, app related files

--- src/angular/app/app-routing.module.ts
+++ src/angular/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { ResetPasswordComponent } from './pages/auth/reset-password/reset-password.component';
+import { VerifyEmailComponent } from './pages/auth/verify-email/verify-email.component';
+import { ProfileComponent } from './pages/users/profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'sign-in',
+        component: AuthComponent
+      },
+      {
+        path: 'sign-out',
+        component: AuthComponent
+      },
+      {
+        path: 'verify-email/:token',
+        component: VerifyEmailComponent
+      },
+      {
+        path: 'reset-forgot-password/:token',
+        component: ResetPasswordComponent
+      },
+      {
+        path: 'reset-password',
+        component: ResetPasswordComponent
+      },
+    ]
+
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/angular/app/app.module.ts
+++ src/angular/app/app.module.ts
@@ -1,18 +1,37 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { CookieService } from 'ngx-cookie-service';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { AuthFormComponent } from './shared/auth-form/auth-form.component';
+import { ResetPasswordComponent } from './pages/auth/reset-password/reset-password.component';
+import { VerifyEmailComponent } from './pages/auth/verify-email/verify-email.component';
+import { ProfileComponent } from './pages/users/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthFormComponent,
+    AuthComponent,
+    ResetPasswordComponent,
+    VerifyEmailComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
