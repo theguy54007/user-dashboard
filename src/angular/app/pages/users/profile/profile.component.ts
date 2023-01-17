265eee0b8a8e4d90468bc5f5ebb89a8f55eab3f2
tommytan <sage.ts920126@gmail.com>
2023-01-17 15:23:17 +0800

    modify profile codes after implement fetching current-user factory

--- src/angular/app/pages/users/profile/profile.component.ts
+++ src/angular/app/pages/users/profile/profile.component.ts
@@ -1,7 +1,7 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { User } from 'src/angular/app/model/user/user.model';
 import { AuthService } from 'src/angular/app/services/auth.service';
 import { UserService } from 'src/angular/app/services/user.service';
@@ -11,9 +11,12 @@ import { UserService } from 'src/angular/app/services/user.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
   user: User;
   form: FormGroup;
+  userSub: Subscription;
+  isLoading = false;
+  isButtonLoading = false;
 
   constructor(
     private authService: AuthService,
@@ -22,35 +25,34 @@ export class ProfileComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.userService.getCurrentUser().subscribe({
+    this.isLoading = true
+    this.userSub = this.authService.user.subscribe({
       next: user => {
         this.user = user
-        this.form = new FormGroup({
-          name: new FormControl(this.user.name, Validators.required)
-        })
-      },
-      error: (err: HttpErrorResponse) => {
-        if (err.status === 403) {
-          return this.authService.redirectToLogin({errorMessage: 'please login before you proceed'})
+
+        if (this.user?.name) {
+          this.form = new FormGroup({
+            name: new FormControl(this.user.name, Validators.required)
+          })
         }
-        const { message } = err.error?.message || {}
-        return this.authService.redirectToLogin({errorMessage: message})
+
+        this.isLoading = false
       }
     })
+  }
 
-
-
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 
   onSubmit(){
     const { name } = this.form.value
-
+    this.isButtonLoading = true
     this.userService.updateName(name).subscribe({
       next: user => {
+        this.isButtonLoading = false
         this.authService.addNewUser(user)
-        setTimeout(() => {
-          this.router.navigate([''], {state: {success: 'Updated successfully!'}})
-        }, 2000);
+        this.router.navigate([''], {state: {success: 'Updated successfully!'}})
       }
     })
   }
