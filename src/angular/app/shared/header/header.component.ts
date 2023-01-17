b1d05f7bac53aabe9dd74bf8b1cc38ae3f643302
tommytan <sage.ts920126@gmail.com>
2023-01-17 15:14:41 +0800

    add header and loading component

---
+++ src/angular/app/shared/header/header.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from '../../model/user/user.model';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  userSub: Subscription
+  user: User
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.user = user
+    });
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+
+  onLogout(){
+    this.authService.signOut().subscribe({
+      next: _ => {
+        this.authService.redirectToLogin({})
+      }
+    })
+  }
+
+}
