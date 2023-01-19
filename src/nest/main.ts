cb6694690eb74d01d68c87d3d3d034c34e04881d
tommytan <sage.ts920126@gmail.com>
2023-01-20 00:17:23 +0800

    install nestjs/swagger library and setup swagger

--- src/nest/main.ts
+++ src/nest/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,15 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser())
+
+  const options = new DocumentBuilder().setTitle('User Dashboard')
+                                       .setDescription('User Dashboard Application')
+                                       .setVersion('1.0')
+                                       .build();
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000);
 }
 bootstrap();
